f0325743cce24f16ab404a9aee36551a290c4644
Jorge Chantryt <jorgemilo51@gmail.com>
2025-05-20 15:49:45 -0500

    Coementarios agregados

--- app/src/main/java/com/example/puravitslab/models/ProductoPedido.kt
+++ app/src/main/java/com/example/puravitslab/models/ProductoPedido.kt
@@ -4,4 +4,11 @@ data class ProductoPedido(
     val nombre: String = "",
     val precio: Double = 0.0,
     val cantidad: Int = 1
+
+
+    /*
+    * Producto que aparece en el carrito y son los productos que se pueden editar
+    * Agregar datos necesarios del producto a editar (por ahora solo balsamo)
+    * En carrito y personalizar diferenciar los balsamos
+    * */
 )
